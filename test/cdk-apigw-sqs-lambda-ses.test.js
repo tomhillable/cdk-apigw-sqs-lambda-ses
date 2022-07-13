21e62c74c6c969ab0185a075566c438d2b0a141b
Tom Hill <tom@clockworkcubed.com>
2022-07-12 21:55:37 -0400

    Initial commit

---
+++ test/cdk-apigw-sqs-lambda-ses.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const CdkApigwSqsLambdaSes = require('../lib/cdk-apigw-sqs-lambda-ses-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-apigw-sqs-lambda-ses-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new CdkApigwSqsLambdaSes.CdkApigwSqsLambdaSesStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});

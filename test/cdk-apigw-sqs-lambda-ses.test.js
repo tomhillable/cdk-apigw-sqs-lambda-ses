772bc04c0663b308e2ebfdba807376ef7a60a607
Tom Hill <tom@clockworkcubed.com>
2022-07-22 17:31:02 -0400

    Add lambda function code

--- test/cdk-apigw-sqs-lambda-ses.test.js
+++ test/cdk-apigw-sqs-lambda-ses.test.js
@@ -1,6 +1,7 @@
 const cdk = require("aws-cdk-lib");
 const { Template, Match } = require("aws-cdk-lib/assertions");
 const CdkApigwSqsLambdaSes = require("../lib/cdk-apigw-sqs-lambda-ses-stack");
+const { createSesParams } = require("../src/lambda");
 
 describe("In the output stack", () => {
   const app = new cdk.App();
@@ -54,5 +55,33 @@ describe("In the output stack", () => {
     test("API has API key", () => {
       template.hasResourceProperties("AWS::ApiGateway::ApiKey", {});
     });
+  });
+});
+
+describe("The lambda function", () => {
+  const record = {
+    version: "1.0.0",
+    name: "Tom Hill",
+    email: "example@example.com",
+  };
+
+  test("can generate valid SES parameters", () => {
+    const params = createSesParams(record);
+    expect(params).toMatchObject(
+      expect.objectContaining({
+        Source: expect.any(String),
+        Destination: { ToAddresses: ["example@example.com"] },
+        Message: {
+          Subject: { Data: expect.any(String) },
+          Body: {
+            Html: expect.objectContaining({ Data: expect.any(String) }),
+            Text: expect.objectContaining({ Data: expect.any(String) }),
+          },
+        },
+      })
+    );
+  });
+  test("rejects records with unknown schema version", () => {
+    expect(() => { createSesParams({version: '3.2.1'}); }).toThrow();
   });
 });
